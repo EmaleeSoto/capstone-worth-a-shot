6dae76ab51e15801c304c3fd3cc14df2850dc511
EmaleeSoto <emaleesoto@gmail.com>
2022-11-15 10:23:49 -0500

    Backend and Frontend setup

---
+++ back-end/app.js
@@ -0,0 +1,23 @@
+// DEPENDENCIES
+
+const express = require("express");
+const cors = require("cors");
+
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to Party Up!");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("Not found!");
+});
+
+// EXPORT
+module.exports = app;
