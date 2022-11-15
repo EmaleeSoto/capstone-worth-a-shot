24fbdbce31155ff7552d8264e0ae1768890bf16c
EmaleeSoto <emaleesoto@gmail.com>
2022-11-15 10:53:52 -0500

    Backend setup

---
+++ back-end/db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS worth_a_shot_user;
+CREATE DATABASE worth_a_shot_user; 
+
+\c worth_a_shot_user; 
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INT NOT NULL,
+    zip_code INT,
+    gender TEXT
+);
