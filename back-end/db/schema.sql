1e67ee6c02ef8ce2c142ad06487907ebf6f07618
EmaleeSoto <emaleesoto@gmail.com>
2022-11-18 05:45:02 -0500

    Added extra user parameters, all is working

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -7,6 +7,9 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     age INT NOT NULL,
+    gender TEXT,
     zip_code TEXT,
-    gender TEXT
+    personality TEXT,
+    flavor TEXT,
+    atmosphere TEXT NOT NULL
 );
