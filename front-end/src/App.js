bf00c02dc92e1ce486213be7e94f472c16882845
Matt M <Afrodesiacc@gmail.com>
2022-11-19 23:57:18 -0500

    Commiting changes to my branch; added a user index based off of the dummy backend.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home'
-import NavBar from './Components/NavBar'
+import Establishments from './Components/Establishments';
+import UserIndex from './Components/UserIndex';
+
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/places" element={<Establishments />}/>
+          <Route path="/users" element={<UserIndex />}/>
         </Routes>
       </Router>
     </div>
