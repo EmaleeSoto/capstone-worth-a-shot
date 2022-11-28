edb5144e7634e8f8af2c478077a25d77cad905a8
Jonathan Scheiber <jon@scheiber.me>
2022-11-28 15:05:12 -0500

    update home content and styling

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,22 +1,21 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div>
-      <h1>This is Worth a Shot!</h1>
-      <section></section>
-      <h5>
+      <h1 className="home-header">This is Worth a Shot!</h1>
+      <p className="home-text">
         Welcome to Worth a Shot, an app designed to help the user interact with
         their peers through one of the world's favorite pastimes.
-      </h5>
-      <section></section>
-      <h4>
+      </p>
+      <p className="home-text">
         With this tool, the User will be able to create an account, choose a
         preferred ambience, vibe, or scene, and be given locations in their area
         which fit the requirements. Similar establishments will be curated and
         presented to the User as well, allowing them more control over the
         direction of their night {"(or Day, should that be your energy!)"}.
-      </h4>
+      </p>
     </div>
   );
 };
