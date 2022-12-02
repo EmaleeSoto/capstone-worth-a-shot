f10250865b86937d323dd993b1f917cecb2083b6
Jonathan Scheiber <jon@scheiber.me>
2022-12-02 03:28:30 -0500

    add styling to landing

--- front-end/src/Pages/LandingSignedIn.js
+++ front-end/src/Pages/LandingSignedIn.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./LandingSignedIn.css";
+
 const LandingPageSignedIn = ({ user }) => {
   const [loggedInUser, setLoggedInUser] = useState({});
   useEffect(() => {
     setLoggedInUser(user);
   }, [user]);
   return (
-    <div>
+    <div className="landing-container">
       <h1>Hi {loggedInUser.name}, what are your plans tonight?</h1>
-      <break></break>
-      <Link to={`/establishments`}>Find Clubs</Link>
-      <br></br>
-      <Link to="/alcohols">Find Drinks</Link>
+      <Link to={`/establishments`}>
+        <button className="large-button">Find Clubs</button>
+      </Link>
+      <br />
+      <Link to="/alcohols">
+        <button className="large-button">Find Drinks</button>
+      </Link>
     </div>
   );
 };
