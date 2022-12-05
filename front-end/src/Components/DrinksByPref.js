b977c937a2ac07015d6a428b0a51be5f223a6d12
Jonathan Scheiber <jon@scheiber.me>
2022-12-05 03:10:22 -0500

    update drink styling

--- front-end/src/Components/DrinksByPref.js
+++ front-end/src/Components/DrinksByPref.js
@@ -2,12 +2,13 @@ import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
+import "./DrinksByPref.css";
 const API = process.env.REACT_APP_API_URL;
 
 const DrinksByPref = ({ user }) => {
   const [allDrinks, setAllDrinks] = useState([]);
   const [displayDrinks, setDisplayDrinks] = useState([]);
-  const [userTastes, setUserTastes] = useState([])
+  const [userTastes, setUserTastes] = useState([]);
 
   const [loggedInUser, setLoggedInUser] = useState({});
   useEffect(() => {
@@ -26,21 +27,41 @@ const DrinksByPref = ({ user }) => {
       });
   }, [loggedInUser]);
 
-  
   return (
     <div>
-     <Link to="/alcohols/category/beer"><button value="beer">Beer!</button></Link>
-      <br/>
-      <Link to="/alcohols/category/cider"><button value="cider">Cider!</button></Link>
-      <br/>
-      <Link to="/alcohols/category/wine"><button value="wine">Wine!</button></Link>
-      <br/>
-      <Link to="/alcohols/category/whiskey"><button value="whiskey">Whiskey!</button></Link>
-      <br/>
-      <Link to="/alcohols/category/gin"><button value="gin">Gin!</button></Link>
-      <br/>
-      <Link to="/alcohols/category/brandy"><button value="brandy">Brandy!</button></Link>
-      <br/>
+      <h1 className="drink-header">What kind of drink are you looking for?</h1>
+      <section className="drink-category-grid">
+        <Link to="/alcohols/category/beer">
+          <button className="drink-category-button" value="beer">
+            Beer!
+          </button>
+        </Link>
+        <Link to="/alcohols/category/cider">
+          <button className="drink-category-button" value="cider">
+            Cider!
+          </button>
+        </Link>
+        <Link to="/alcohols/category/wine">
+          <button className="drink-category-button" value="wine">
+            Wine!
+          </button>
+        </Link>
+        <Link to="/alcohols/category/whiskey">
+          <button className="drink-category-button" value="whiskey">
+            Whiskey!
+          </button>
+        </Link>
+        <Link to="/alcohols/category/gin">
+          <button className="drink-category-button" value="gin">
+            Gin!
+          </button>
+        </Link>
+        <Link to="/alcohols/category/brandy">
+          <button className="drink-category-button" value="brandy">
+            Brandy!
+          </button>
+        </Link>
+      </section>
     </div>
   );
 };
