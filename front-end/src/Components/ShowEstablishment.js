6ea562b88fe6b391f278e2175fac079162732ee6
Matt M <Afrodesiacc@gmail.com>
2022-12-06 11:02:29 -0500

    added favorite list. Clickable; bringing one to the establishment page.

--- front-end/src/Components/ShowEstablishment.js
+++ front-end/src/Components/ShowEstablishment.js
@@ -21,7 +21,7 @@ export default function ShowEstablishment({ user }) {
       .catch(() => {
         navigate("/not found");
       });
-  }, [id]);
+  }, [id, navigate]);
 
   const militaryToUSD = (time) => {
     let hrs = String(time).substring(0, 2);
